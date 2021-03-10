07f04ebcd9ae478a66bba0de01a36611ef97e458
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-03-10 14:44:47 +0100

    phaser intro af

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var game;
 window.onload = function () {
     var config = {
         type: Phaser.AUTO,
-        width: 480,
+        width: 1280,
         height: 640,
         parent: 'phaser-game',
         backgroundColor: Phaser.Display.Color.HexStringToColor("#f7e167"),
